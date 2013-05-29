b626d857177426afc49ec3889a1cd158ff08bbcc
Brian Olore <brian@olore.net>
2013-05-28 17:30:08 -0700

    extract partials & use ng-include

--- ui/app/scripts/controllers/user.js
+++ ui/app/scripts/controllers/user.js
@@ -3,16 +3,17 @@
 angular.module('uiApp')
   .controller('UserCtrl', function ($scope) {
 
+    //TODO do something with currentUser... like use it to determine
+    //whether or not to show the login form
+
     $scope.authenticate = function(username, password) {
-      var user = $scope.findUser(username);
+      $scope.currentUser = $scope.findUser(username);
       
-      if (password == user.password) {
-        console.log("yep");
+      if (password == $scope.currentUser.password) {
         $('#fail').hide();
         $('.login').replaceWith("<h4 class='welcome'>Welcome " + username + "</h4>");
         return true;
       } else {
-        console.log("nope");
         $('#fail').show();
         $scope.password = '';
         return false;
