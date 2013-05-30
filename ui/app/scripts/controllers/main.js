8818699be6af74f6ae01d7fb7385a613c5c8df3b
Brian Olore <brian@olore.net>
2013-05-30 16:54:59 -0700

    refactored

--- ui/app/scripts/controllers/main.js
+++ ui/app/scripts/controllers/main.js
@@ -1,15 +1,14 @@
 'use strict';
 
 angular.module('uiApp')
-  .controller('MainCtrl', function ($scope, User) {
+  .controller('MainCtrl', function ($scope, UserService) {
 
     //child scopes can't inherit primitives 
     //http://stackoverflow.com/a/14234367/1691
     $scope.userLogin = { };
 
     $scope.login = function() {
-      var user = User.find($scope.userLogin.username);
-
+      var user = UserService.find($scope.userLogin.username);
       if ($scope.userLogin.password === user.password) {
         $scope.currentUser = user;
         $scope.loginFailed = false;
---
+++ ui/app/scripts/services/user.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('uiApp').factory("UserService", function() {
+
+  return {
+
+    find: function(username) {
+      //TODO: Do some HTTP call & get some JSON & turn it into a User
+      return new User({ username: username, 
+                        password: 'admin'  });
+    }
+
+  }
+
+});
